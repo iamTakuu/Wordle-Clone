b0dbd93fbe6afbd3ee51694459ebdd9ead2fb451
Taku <marowatk@gmail.com>
2022-07-31 16:04:20 +0200

    Initial commit

---
+++ Assets/_Scripts/BGScroller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts
+{
+    public class BGScroller : MonoBehaviour
+    {
+        [SerializeField] private RawImage _rawImage;
+        [SerializeField] private float _x, _y;
+        void Update()
+        {
+            _rawImage.uvRect = new Rect(_rawImage.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _rawImage.uvRect.size);
+        }
+    }
+}
