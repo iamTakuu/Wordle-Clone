408e9687fea63de5004b2192c2e9acb25c27f6de
Taku <marowatk@gmail.com>
2022-08-02 20:56:59 +0200

    Final whoops-

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Image inputIMG;
     [SerializeField] private GameObject rulesPanel;
     [SerializeField] private TextMeshProUGUI attemptsText;
+    [SerializeField] private TextMeshProUGUI dubText;
     private string randomWord;
     private string userInput;
     private int attempts;
@@ -117,6 +118,7 @@ public class GameManager : MonoBehaviour
         else
         {
             AnimateRight();
+            dubText.transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0), 1f, 1, 0.2f).SetEase(Ease.InBounce).OnComplete((() => dubText.transform.DOScale(Vector3.zero, 0.2f).SetEase(Ease.InOutBounce)));
             ResetGame(true);
         }
         
